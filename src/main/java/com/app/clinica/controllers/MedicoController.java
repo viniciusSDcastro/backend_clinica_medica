51afd4730ae3ee37823166bd531235f6572a1aaa
vinicius <silvadecastrovinicius9@gmail.com>
2024-03-31 15:04:23 -0300

    organizei a classe MedicoController

--- src/main/java/com/app/clinica/controllers/MedicoController.java
+++ src/main/java/com/app/clinica/controllers/MedicoController.java
@@ -1,32 +1,16 @@
 package com.app.clinica.controllers;
-// import java.util.List;
 
-// import java.util.Optional;
-// import java.util.UUID;
-
-//import org.slf4j.Logger;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.clinica.dto.MedicoRecordDTO;
 import com.app.clinica.models.MedicoModel;
 import com.app.clinica.repositories.MedicoRepository;
-
-// import com.clinica.app.dto.MedicoRecordDTO;
-// import com.clinica.app.models.MedicoModel;
-// import com.clinica.app.repositories.MedicoRepository;
 
 import jakarta.validation.Valid;
 
